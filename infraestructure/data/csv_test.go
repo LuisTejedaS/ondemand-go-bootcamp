89180fd879196a5763d7dfc0221e477544aa339e
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-03-17 18:00:07 -0600

    add test
    add test for csv package

--- infraestructure/data/csv_test.go
+++ infraestructure/data/csv_test.go
@@ -1,8 +1,33 @@
 package data
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+func createFile(t *testing.T) string {
+	file, fe := os.Create("testdata/poke.csv")
+	if fe != nil {
+		t.Errorf("could not create file for testing: %s", fe)
+	}
+	dir, pe := filepath.Abs(filepath.Dir(file.Name()))
+	if pe != nil {
+		t.Errorf("Error getting file path: %s", pe)
+	}
+	return dir
+}
+
+func populateFile(t *testing.T, p string) {
+	record := []string{"1", "Charmander"}
+	header := []string{"id", "Name"}
+	csvDSt, _ := NewCSVDataStore(p)
+	csvDSt.SaveRecord(header)
+	err := csvDSt.SaveRecord(record)
+	if err != nil {
+		t.Errorf("There was an error saving record: %s", err)
+	}
+}
 
 func TestCreateNewCSVDataSource(t *testing.T) {
 	p := "ondemand-go-bootcamp/poke.csv"
@@ -34,4 +59,47 @@ func TestCreateNewCSVDataStoreInvalidPath(t *testing.T) {
 	if err == nil {
 		t.Errorf("Expected error with invalid path set: %s, as path", p)
 	}
+}
+
+func TestReadCollection(t *testing.T) {
+	dir := createFile(t)
+	p := dir + "/poke.csv"
+	populateFile(t, p)
+	csvDS, _ := NewCSVDataSource(p)
+	if csvDS.csvPath != p {
+		t.Errorf("CSVPath was not set, got: %s, want: %s.", csvDS.csvPath, p)
+	}
+	pok, err := csvDS.ReadCollection()
+	if err != nil {
+		t.Errorf("Error reading collection %s", err)
+	}
+	if len(pok) != 1 {
+		t.Errorf("Expected to read: %d pokemons but read %d", 1, len(pok))
+	}
+	os.Remove("testdata/poke.csv")
+}
+
+func TestSaveRecord(t *testing.T) {
+	dir := createFile(t)
+	p := dir + "/poke.csv"
+	record := []string{"1", "Charmander"}
+	header := []string{"id", "Name"}
+	csvDSt, _ := NewCSVDataStore(p)
+	csvDSt.SaveRecord(header)
+	err := csvDSt.SaveRecord(record)
+	if err != nil {
+		t.Errorf("There was an error saving record: %s", err)
+	}
+	csvDS, _ := NewCSVDataSource(p)
+	if csvDS.csvPath != p {
+		t.Errorf("CSVPath was not set, got: %s, want: %s.", csvDS.csvPath, p)
+	}
+	pok, err := csvDS.ReadCollection()
+	if err != nil {
+		t.Errorf("Error reading collection %s", err)
+	}
+	if len(pok) != 1 {
+		t.Errorf("Expected to read: %d pokemons but read %d", 1, len(pok))
+	}
+	os.Remove("testdata/poke.csv")
 }
